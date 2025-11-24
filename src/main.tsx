e16cd6f64ef6d1aed7019dd3024b874f8f4590f2
MathiusCp <castillomateous@gmail.com>
2025-11-24 08:28:13 -0500

    organizacion

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./styles/global.css";
+import "./styles/login-register.css";
+import "./styles/footer.css"
+import "./styles/lessons-page.css";
+import "./styles/challenges-page.css";
+import "./styles/pages/lesson.css";
+import "./styles/pages/challenges.css";
+import "./styles/pages/perfil.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
