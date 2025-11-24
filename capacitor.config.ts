fc2a4c83625b398bf1d1636ce0dd9d89bc7965c6
MathiusCp <castillomateous@gmail.com>
2025-11-24 11:48:46 -0500

    APK

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.codeseed.app',
+  appName: 'codeseed',
+  webDir: 'dist'
+};
+
+export default config;
